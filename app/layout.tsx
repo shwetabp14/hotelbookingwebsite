9b26c6c5931dbae0f497fa5f2d0005b11dfbdb26
AMI-XYZ <deeptisingh9336@gmail.com>
2024-07-28 23:45:07 +0530

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,51 @@
+import { Nunito } from "next/font/google";
+
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "./components/navbar/Navabar";
+import ClientOnly from "./components/ClientOnly";
+
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
+import LoginModal from "./components/modals/LoginModal";
+import getCurrentUser from "./actions/getCurrentUser";
+import RentModal from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+})
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const currentUser = await getCurrentUser();
+
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <SearchModal/>
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser = {currentUser} />
+        </ClientOnly> 
+        <div className="pb-20 pt-28">
+          {children}
+        </div>
+      </body>
+    </html>
+  );
+}
